1f51b53cc80ac09200ffaa2b4b1305bdec8386bd
ernest <ernest@192.168.127.10>
2015-12-16 20:40:48 -0500

    Avoid inserting duplicate time-stamp records

--- src/jntpplot/Jntpplot.java
+++ src/jntpplot/Jntpplot.java
@@ -26,10 +26,40 @@ public class Jntpplot {
      * @param args the command line arguments
      * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException, SQLException {
         // TODO code application logic here
 
         // http://stackoverflow.com/questions/4008223/print-in-new-line-java
         System.out.println("Hello ntpplot!");
+        
+        String fileName = "test/jntpplot/sys";
+        Stats statsFile = new Stats();
+        statsFile.setFileName(fileName);
+        ArrayList<ArrayList<String>> stats = statsFile.getInjestFile();
+        
+        
+        for (ArrayList<String> message: stats) {
+            Database statsDb = new Database();
+            statsDb.setDbName("stats_test");
+            statsDb.setTableName("sysstats");
+            statsDb.setTableColumns("(date INT," +
+                "time REAL," +
+                "time_since_restart INT," +
+                "packets_recieved INT," +
+                "packats_processed INT," +
+                "current_version INT," +
+                "previous_version INT," +
+                "bad_version INT," +
+                "access_denied INT," +
+                "bad_length_or_format INT," +
+                "bad_authentication INT," +
+                "rate_exceeded INT)");
+            Connection conn = statsDb.openDb();
+            System.out.println(message);
+            statsDb.setStatMessage(message);
+            statsDb.setDbConnection(conn);
+            Boolean result = statsDb.insertStat();
+            System.out.println(result);
+        }
     }
 }
